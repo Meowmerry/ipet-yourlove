790ce88a839edf81a5889d843adb8f4d70a61c0c
thasanee <meow3525@gmail.com>
2021-04-25 02:19:43 +0700

    add gitignore

---
+++ build/precache-manifest.9633e0df77e18be18861802dcd473e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cda2eee1b468a4e04619e9733864e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a04010734cd399bb78d6",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "05a531e2b2d4e4ec455e",
+    "url": "/static/css/main.a6583af0.chunk.css"
+  },
+  {
+    "revision": "a04010734cd399bb78d6",
+    "url": "/static/js/2.8d888447.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.8d888447.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05a531e2b2d4e4ec455e",
+    "url": "/static/js/main.def46cbd.chunk.js"
+  },
+  {
+    "revision": "b5ff61001688404db26f",
+    "url": "/static/js/runtime-main.8c5f780f.js"
+  }
+]);
